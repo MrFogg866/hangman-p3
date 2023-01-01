b691ad76c5150100b40b99c30c13ac3949fb2116
MrFogg866 <philiplynch866@gmail.com>
2023-01-01 14:27:51 +0000

    add cars to word dictionary library

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+print                        ("Welcome to Sports Car Hangman")
+print  ("_____________________________________________________________________________")
+print  ("      .----------.              .----------.              .----------.       ")
+print  ("     /            \            /            \            /            \      ") 
+print  ("   _(.-. _...._ .-.)_        _(.-. _...._ .-.)_        _(.-. _...._ .-.)_    ")
+print  ("  (_)`-' __)(__ `-'(_)      (_)`-' __()__ `-'(_)      (_)`-' __()__ `-'(_)   ") 
+print  (" (....__|MR_FOGG|__....)   (....__|MR_FOGG|__....)   (....__|MR_FOGG|__....) ") 
+print  ("  | |    ~~~~~~    | |      | |    ~~~~~~    | |      | |    ~~~~~~    | |   ") 
+print  ("  `-'              `-'      `-'              `-'      `-'              `-'   ") 
+print  ("_____________________________________________________________________________")
+
+wordDictionary = ["ferrari", "lamborghini", "mclaren", "maserati", "bentley", "porsche", ]
